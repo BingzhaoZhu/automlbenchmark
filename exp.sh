25417c7a8ed2431356a2f35851bca44018096ba6
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-16 19:18:12 +0000

    Added FT_Transformer

--- exp.sh
+++ exp.sh
@@ -8,6 +8,6 @@ cp ./examples/config.yaml ~/.config/automlbenchmark/
 #python runbenchmark.py CAT_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py XGB_AG ag mytest -m aws -p 1500 -f 0
 
-python runbenchmark.py FTTransformer adult mytest -m aws -p 1500
+python runbenchmark.py FTTransformer adult mytest -m aws -p 1500 -f 0
 
 #python runbenchmark.py FTTransformer test mytest -f 0
