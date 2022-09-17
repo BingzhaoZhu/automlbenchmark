d93a68a6f39d84adae8d54e1365d52b4b1c0fcaf
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-17 01:24:33 +0000

    Added FT_Transformer

--- exp.sh
+++ exp.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_cpu.yaml ~/.config/automlbenchmark/config.yaml
+cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
 #python runbenchmark.py GBM_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py RF_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py CAT_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py XGB_AG ag mytest -m aws -p 1500 -f 0
 
-python runbenchmark.py FTTransformer_cpu adult mytest -m aws -p 1500 -f 0
+python runbenchmark.py FTTransformer_gpu adult mytest -m aws -p 150 -f 0
 
 #python runbenchmark.py FTTransformer test mytest -f 0
