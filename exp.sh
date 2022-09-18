708cad64873e0e51e76da593600995a7869188fa
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-18 06:15:25 +0000

    Added FT_Transformer

--- exp.sh
+++ exp.sh
@@ -8,7 +8,7 @@ rm -f ~/.config/automlbenchmark/config.yaml
 #python runbenchmark.py CAT_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py XGB_AG ag mytest -m aws -p 1500 -f 0
 
-cp ./examples/config_cpu.yaml ~/.config/automlbenchmark/config.yaml
+cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 python runbenchmark.py FTTransformer_gpu_pretrain1 ag mytest -m aws -p 150 -f 0
 python runbenchmark.py FTTransformer_gpu_pretrain2 ag mytest -m aws -p 150 -f 0
 python runbenchmark.py FTTransformer_gpu_pretrain3 ag mytest -m aws -p 150 -f 0
