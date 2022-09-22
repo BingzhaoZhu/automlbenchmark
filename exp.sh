483cb6e8f5df46e59e7576de7c4ff1e87389ce68
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-22 21:49:20 +0000

    Added Pretrain

--- exp.sh
+++ exp.sh
@@ -13,15 +13,17 @@ rm -f ~/.config/automlbenchmark/config.yaml
 
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 #python runbenchmark.py FTT_AG ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTT_AG_pretrain ag mytest -m aws -p 150 -f 0
 
-python runbenchmark.py FTTransformer_gpu_1 ag mytest -m aws -p 150 -f 0
-python runbenchmark.py FTTransformer_gpu_pretrain_1 ag mytest -m aws -p 150 -f 0
 
-python runbenchmark.py FTTransformer_gpu_3 ag mytest -m aws -p 150 -f 0
-python runbenchmark.py FTTransformer_gpu_pretrain_3 ag mytest -m aws -p 150 -f 0
-
-python runbenchmark.py FTTransformer_gpu_5 ag mytest -m aws -p 150 -f 0
-python runbenchmark.py FTTransformer_gpu_pretrain_5 ag mytest -m aws -p 150 -f 0
-
-python runbenchmark.py WideDeep ag mytest -m aws -p 150 -f 0
-python runbenchmark.py WideDeep_pretrain ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTTransformer_gpu_1 ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTTransformer_gpu_pretrain_1 ag mytest -m aws -p 150 -f 0
+#
+#python runbenchmark.py FTTransformer_gpu_3 ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTTransformer_gpu_pretrain_3 ag mytest -m aws -p 150 -f 0
+#
+#python runbenchmark.py FTTransformer_gpu_5 ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTTransformer_gpu_pretrain_5 ag mytest -m aws -p 150 -f 0
+#
+#python runbenchmark.py WideDeep ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py WideDeep_pretrain ag mytest -m aws -p 150 -f 0
