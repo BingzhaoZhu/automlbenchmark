56987c6571b652b527d5ef503fc9dd9d2dbb6961
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-22 19:31:30 +0000

    Added Pretrain

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
-REPO=${2:-"https://github.com/awslabs/autogluon.git"}
+REPO=${2:-"https://github.com/BingzhaoZhu/autogluon.git"}
 PKG=${3:-"autogluon"}
 
 
@@ -19,9 +19,9 @@ PIP install --upgrade setuptools wheel
 
 if [[ "$VERSION" == "latest_gpu" ]]; then
     PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
-    VERSION="master"
+    VERSION="pretrain"
 elif [[ "$VERSION" == "latest" ]]; then
-    VERSION="master"
+    VERSION="pretrain"
 fi
 
 if [[ "$VERSION" == "stable" ]]; then
