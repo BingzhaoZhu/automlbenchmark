6ddc4194b80bc252f761160d3b002ed7aa045b08
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-27 05:38:19 +0000

    Added Pretrain

--- exp.sh
+++ exp.sh
@@ -4,7 +4,7 @@ rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_cpu.yaml ~/.config/automlbenchmark/config.yaml
 #python runbenchmark.py GBM_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py RF_AG ag mytest -m aws -p 1500 -f 0
-#python runbenchmark.py CAT_AG ag mytest -m aws -p 1500 -f 0
+python runbenchmark.py CAT_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py XGB_AG ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py CAT_AG_pretrain ag mytest -m aws -p 1500 -f 0
 #python runbenchmark.py NN_AG ag mytest -m aws -p 1500 -f 0
@@ -14,10 +14,10 @@ cp ./examples/config_cpu.yaml ~/.config/automlbenchmark/config.yaml
 python runbenchmark.py CAT_AG_pretrain ag mytest -m aws -p 1500 -f 0
 
 #sleep 3600
-rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
+#rm -f ~/.config/automlbenchmark/config.yaml
+#cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 #python runbenchmark.py FTT_AG ag mytest -m aws -p 150 -f 0
-python runbenchmark.py FTT_AG_pretrain ag mytest -m aws -p 150 -f 0
+#python runbenchmark.py FTT_AG_pretrain ag mytest -m aws -p 150 -f 0
 
 
 #python runbenchmark.py FTTransformer_gpu_1 ag mytest -m aws -p 150 -f 0
