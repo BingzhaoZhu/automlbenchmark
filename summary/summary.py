a1314c7bc1357c6c4651a567788fe8cc637a01d7
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-09-27 03:57:43 +0000

    Added Pretrain

--- summary/summary.py
+++ summary/summary.py
@@ -9,7 +9,7 @@ locations = {
             # "WideDeep": "widedeep.ag.mytest.aws.20220921T180925/",
             # "WideDeep_pretrain": "widedeep_pretrain.ag.mytest.aws.20220921T172633/",
             "CAT": "cat_ag.ag.mytest.aws.20220926T055336/",
-            "CAT_pretrain": "cat_ag_pretrain.ag.mytest.aws.20220927T010634/",
+            "CAT_pretrain": "cat_ag_pretrain.ag.mytest.aws.20220927T022339/",
             # "LGBM": "gbm_ag.ag.mytest.aws.20220917T173005/",
             # "RF": "rf_ag.ag.mytest.aws.20220917T181110/",
             # "XGB": "xgb_ag.ag.mytest.aws.20220917T202434/",
