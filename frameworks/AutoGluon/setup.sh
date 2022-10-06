40c8c32d08f5097b51afd77d4869638d0388b347
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-06 23:29:26 +0000

    Added additive attention (linear complexity) to transformer models.

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -16,12 +16,15 @@ fi
 
 PIP install --upgrade pip
 PIP install --upgrade setuptools wheel
+PIP install mmcv
 
 if [[ "$VERSION" == "latest_gpu" ]]; then
     PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
-    VERSION="pretrain"
+    # VERSION="pretrain"
+    VERSION="master"
 elif [[ "$VERSION" == "latest" ]]; then
-    VERSION="pretrain"
+    # VERSION="pretrain"
+    VERSION="master"
 fi
 
 if [[ "$VERSION" == "stable" ]]; then
