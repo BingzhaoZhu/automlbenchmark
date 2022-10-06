692b36d4d3eb96b2ca30f9023389236a63fe0c54
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-06 00:06:39 +0000

    Added row_attention

--- exp.sh
+++ exp.sh
@@ -17,13 +17,14 @@
 rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 #python runbenchmark.py FTT_AG ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FTT_AG_pretrain_identical ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FTT_AG_pretrain_identical ag mytest -m aws -p 104 -f 0
 python runbenchmark.py FTT_AG_pretrain_randperm_03 ag mytest -m aws -p 104 -f 0
-#python runbenchmark.py FTT_AG_pretrain_randperm_06 ag mytest -m aws -p 104 -f 0
+python runbenchmark.py FTT_AG_pretrain_randperm_06 ag mytest -m aws -p 104 -f 0
 #python runbenchmark.py FTT_AG_pretrain_randperm_09 ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FTT_AG_pretrain_randblk_03 ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FTT_AG_pretrain_randblk_06 ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FTT_AG_pretrain_randblk_09 ag mytest -m aws -p 104 -f 0
+
+#python runbenchmark.py FTT_AG_pretrain_randblk_03 ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FTT_AG_pretrain_randblk_06 ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FTT_AG_pretrain_randblk_09 ag mytest -m aws -p 104 -f 0
 
 #python runbenchmark.py FTT_AG_row_attention ag mytest -m aws -p 104 -f 0
 
