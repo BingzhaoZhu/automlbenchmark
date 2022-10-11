d051f4175b11673d47f14e22e1b269bd57f9d6a3
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-11 22:27:01 +0000

    Added filters to remove nan in output.

--- exp.sh
+++ exp.sh
@@ -14,13 +14,13 @@
 #python runbenchmark.py CAT_AG_pretrain ag mytest -m aws -p 1500 -f 0
 
 #sleep 3600
-rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
+#rm -f ~/.config/automlbenchmark/config.yaml
+#cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py FTT_AG ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FastFTT_AG_32 ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FTT_AG_32 ag mytest -m aws -p 104 -f 0
-python runbenchmark.py FastFTT_AG ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FTT_AG ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FastFTT_AG_32 ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FTT_AG_32 ag mytest -m aws -p 104 -f 0
+#python runbenchmark.py FastFTT_AG ag mytest -m aws -p 104 -f 0
 
 #python runbenchmark.py FTT_AG_pretrain_identical ag mytest -m aws -p 104 -f 0
 #python runbenchmark.py FTT_AG_pretrain_randperm_03 ag mytest -m aws -p 104 -f 0
