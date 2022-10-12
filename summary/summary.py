73be861e97ec669626ac165a5f3b9ef3c60e5473
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-12 23:42:13 +0000

    fixed parameter init

--- summary/summary.py
+++ summary/summary.py
@@ -19,7 +19,7 @@ locations = {
             "FTT": "ftt_ag.ag.mytest.aws.20221011T031621/",
             "FastFTT": "fastftt_ag.ag.mytest.aws.20221012T060457/",
             "FTT_batchsize_32": "ftt_ag_32.ag.mytest.aws.20221011T080250/",
-            "FastFTT_batchsize_32":"fastftt_ag_32.ag.mytest.aws.20221012T041432/",
+            "FastFTT_batchsize_32": "fastftt_ag_32.ag.mytest.aws.20221012T181213/",
             # "HTT": "htt_ag.ag.mytest.aws.20221006T045542/",
 
             # "FTT_pretrain_identical": "ftt_ag_pretrain_identical.ag.mytest.aws.20220928T200551/",
