2daa50556d11fcd0eaa2606a36719d670a0c51b1
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-10-31 04:42:02 +0000

    automatically apply FastFTT when columns > 300

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -21,10 +21,11 @@ PIP install pulp
 if [[ "$VERSION" == "latest_gpu" ]]; then
     PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
     VERSION="pretrain"
-#    VERSION="master"
+elif [[ "$VERSION" == "latest_master" ]]; then
+    PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
+    VERSION="master"
 elif [[ "$VERSION" == "latest" ]]; then
     VERSION="pretrain"
-#    VERSION="master"
 fi
 
 if [[ "$VERSION" == "stable" ]]; then
