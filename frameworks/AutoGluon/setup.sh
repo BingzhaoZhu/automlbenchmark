63fac80b1f29ca976c03bb26b65f0006a965814b
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-08 19:12:25 +0000

    added pretraining

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -25,7 +25,9 @@ elif [[ "$VERSION" == "latest_master" ]]; then
     PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
     VERSION="master"
 elif [[ "$VERSION" == "latest" ]]; then
-    VERSION="refrac"
+    VERSION="refrac_cpu"
+elif [[ "$VERSION" == "pretrain" ]]; then
+    VERSION="pretrain"
 fi
 
 if [[ "$VERSION" == "stable" ]]; then
