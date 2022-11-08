de2cd2920d34d8216ccb3b4f171d810cbe05b0ad
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-08 18:31:35 +0000

    added pretraining

--- exp_cpu.sh
+++ exp_cpu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_cpu.yaml ~/.config/automlbenchmark/config.yaml
+cp ./examples/config_m6i.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py Ensemble_AG_bq ag mytest24h -m aws -p 1040
+python runbenchmark.py Ensemble_AG_mq adult mytest -m aws -p 1040
