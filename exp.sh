99c84a21542f898ce5e628e4acf6110b2c5eadb2
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-11 23:53:39 +0000

    Auto-switch to additive attention for FTT

--- exp.sh
+++ exp.sh
@@ -3,3 +3,4 @@
 rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
+python runbenchmark.py Ensemble_AG_FastFTT_bq ag mytest4h -m aws -p 180
--- summary/summary.py
+++ summary/summary.py
@@ -35,12 +35,17 @@ locations = {
             # "ensemble_FTT_pretrain": "ensemble_ag_ftt_pretrain.ag.mytest4h.aws.20221027T233830/",
             # "ensemble_ag_ftt_all": "ensemble_ag_ftt_all.ag.mytest4h.aws.20221031T211433/",
 
-            "ensemble_bq": "ensemble_ag_bq.ag.mytest24h.aws.20221105T052819/",
-            "ensemble_bq_m6i": "ensemble_ag_bq.ag.mytest24h.aws.20221109T020748/",
-            # "ensemble_bq": "ensemble_ag_bq.ag.mytest4h.aws.20221102T230938/",
-            # "ensemble_FTT_pretrain_bq": "ensemble_ag_ftt_pretrain_bq.ag.mytest4h.aws.20221103T051502/",
-            "ensemble_ag_ftt_all_bq": "ensemble_ag_ftt_all_bq.ag.mytest24h.aws.20221105T061959/", #"ensemble_ag_ftt_all_mq.ag.mytest4h.aws.20221104T174422/",
-            "ensemble_ag_ftt_all_bq_m6i": "ensemble_ag_ftt_all_bq_cpu.ag.mytest24h.aws.20221109T020548/",
+            # "ensemble_bq": "ensemble_ag_bq.ag.mytest24h.aws.20221105T052819/",
+            # "ensemble_bq_m6i": "ensemble_ag_bq.ag.mytest24h.aws.20221109T020748/",
+            # # "ensemble_bq": "ensemble_ag_bq.ag.mytest4h.aws.20221102T230938/",
+            # # "ensemble_FTT_pretrain_bq": "ensemble_ag_ftt_pretrain_bq.ag.mytest4h.aws.20221103T051502/",
+            # "ensemble_ag_ftt_all_bq": "ensemble_ag_ftt_all_bq.ag.mytest24h.aws.20221105T061959/", #"ensemble_ag_ftt_all_mq.ag.mytest4h.aws.20221104T174422/",
+            # "ensemble_ag_ftt_all_bq_m6i": "ensemble_ag_ftt_all_bq_cpu.ag.mytest24h.aws.20221109T020548/",
+
+            "ensemble_bq": "ensemble_ag_bq.ag.mytest4h.aws.20221102T230938/",
+            "ensemble_fastftt_bq": "ensemble_ag_fastftt_bq.ag.mytest4h.aws.20221110T232827/",
+            "ensemble_autoftt_bq": "ensemble_ag_autoftt_bq.ag.mytest4h.aws.20221110T232806/",
+            "ensemble_ftt_bq": "ensemble_ag_accurateftt_bq.ag.mytest4h.aws.20221110T232734/",
 
             # "FTT_dist": "ftt_ag_pretrain_dist.ag.mytest.aws.20221027T170021/",
             # "FTT_cont": "ftt_ag_pretrain_cont.ag.mytest.aws.20221027T035040/",
@@ -73,7 +78,7 @@ models = ["FTT", "FTT_row_attention_1_gt", "FTT_row_attention_10_gt", "FTT_row_a
 models = ["FTT", "FTT_recon"]
 models = ["FTT", "FTT_cont", "FTT_recon", "FTT_both", "FTT_dist"]
 models = ["ensemble", "ensemble_FTT", "ensemble_FastFTT", "ensemble_FTT_row", "ensemble_FTT_pretrain", "ensemble_ag_ftt_all"]
-models = ["ensemble_bq_m6i", "ensemble_ag_ftt_all_bq"] #, ensemble_bq, ensemble_FTT_pretrain_bq, ensemble_ag_ftt_all_bq]
+models = ["ensemble_autoftt_bq", "ensemble_ftt_bq"] #, "ensemble_autoftt_bq", "ensemble_ftt_bq"] #, ensemble_bq, ensemble_FTT_pretrain_bq, ensemble_ag_ftt_all_bq]
 
 
 s3_client = boto3.client('s3')
