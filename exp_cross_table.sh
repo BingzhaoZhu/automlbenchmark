3bdfcc9dab4345358c555f624953f69c5ad8f8e5
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-20 04:05:36 +0000

    support cross table

--- exp_cross_table.sh
+++ exp_cross_table.sh
@@ -5,11 +5,11 @@ cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
 python cross_table_start.py
 python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
-python cross_table_backbone_update.py
-python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
-python cross_table_backbone_update.py
-python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
-python cross_table_backbone_update.py
-python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
-python cross_table_backbone_update.py
-python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
+#python cross_table_backbone_update.py
+#python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
+#python cross_table_backbone_update.py
+#python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
+#python cross_table_backbone_update.py
+#python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
+#python cross_table_backbone_update.py
+#python runbenchmark.py FTT_AG ag mytest1h -m aws -p 104 -f 0
