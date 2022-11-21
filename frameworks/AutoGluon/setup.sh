bb3814fef9c37038d897874dea423cbf9dea2397
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-21 19:52:39 +0000

    support cross table

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -29,7 +29,7 @@ elif [[ "$VERSION" == "latest" ]]; then
 elif [[ "$VERSION" == "pretrain" ]]; then
     VERSION="pretrain"
 elif [[ "$VERSION" == "cross_table" ]]; then
-    PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
+    # PIP install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchtext==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu113
     VERSION="cross_table_soup"
 fi
 
