3eece00d8c7698c586b0c02b06220e6b4ae8e0ae
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-26 20:10:19 +0000

    support cross table

--- hog_start.py
+++ hog_start.py
@@ -10,10 +10,18 @@ for object in response['Contents']:
     print('Deleting', object['Key'])
     s3_client.delete_object(Bucket=BUCKET, Key=object['Key'])
 
-import json
-job_status = {}
-with open('./job_status.txt', 'w') as fp:
-    fp.write(json.dumps(job_status))
-s3 = boto3.resource('s3')
-s3.Bucket('automl-benchmark-bingzzhu').upload_file('./job_status.txt',
-                                                   'ec2/2022_09_14/cross_table_pretrain/job_status.txt')
+s3_client = boto3.client('s3')
+response = s3_client.list_objects_v2(Bucket='automl-benchmark-bingzzhu',
+                                     Prefix='ec2/2022_09_14/cross_table_pretrain/job_status/'
+                                     )
+for object in response['Contents']:
+    print('Deleting', object['Key'])
+    s3_client.delete_object(Bucket='automl-benchmark-bingzzhu', Key=object['Key'])
+
+# import json
+# job_status = {}
+# with open('./job_status.txt', 'w') as fp:
+#     fp.write(json.dumps(job_status))
+# s3 = boto3.resource('s3')
+# s3.Bucket('automl-benchmark-bingzzhu').upload_file('./job_status.txt',
+#                                                    'ec2/2022_09_14/cross_table_pretrain/job_status.txt')
