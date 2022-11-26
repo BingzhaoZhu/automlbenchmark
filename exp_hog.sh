f0c70d361d942bf8c68411b1b4b9a8a08e37bc2e
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-26 08:06:16 +0000

    support cross table

---
+++ exp_hog.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -f ~/.config/automlbenchmark/config.yaml
+cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
+
+python hog_start.py
+python runbenchmark.py FTT_AG_hog ag mytest1h -m aws -p 104 -f 0
---
+++ hog_start.py
@@ -0,0 +1,19 @@
+import boto3
+
+s3_client = boto3.client('s3')
+
+BUCKET = 'automl-benchmark-bingzzhu'
+
+PREFIX = 'ec2/2022_09_14/cross_table_pretrain/pretrained_hogwild.ckpt'
+response = s3_client.list_objects_v2(Bucket=BUCKET, Prefix=PREFIX)
+for object in response['Contents']:
+    print('Deleting', object['Key'])
+    s3_client.delete_object(Bucket=BUCKET, Key=object['Key'])
+
+import json
+job_status = {}
+with open('./job_status.txt', 'w') as fp:
+    fp.write(json.dumps(job_status))
+s3 = boto3.resource('s3')
+s3.Bucket('automl-benchmark-bingzzhu').upload_file('./job_status.txt',
+                                                   'ec2/2022_09_14/cross_table_pretrain/job_status.txt')
