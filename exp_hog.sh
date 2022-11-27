b2a4b392cdb6baae0171fadc7fcabff5315db6ee
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-27 08:24:29 +0000

    support cross table

--- exp_hog.sh
+++ exp_hog.sh
@@ -4,4 +4,4 @@ rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
 python hog_start.py
-python runbenchmark.py FTT_AG_hog ag mytest1h -m aws -p 104 -f 0
+python runbenchmark.py FTT_AG_hog ag mytest8h -m aws -p 104 -f 0
