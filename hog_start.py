856ccfcbde909b346f779f3913450e53937b7128
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-11-28 09:21:40 +0000

    support cross table

--- hog_start.py
+++ hog_start.py
@@ -12,12 +12,12 @@ for object in response['Contents']:
 
 s3_client = boto3.client('s3')
 response = s3_client.list_objects_v2(Bucket='automl-benchmark-bingzzhu',
-                                     Prefix='ec2/2022_09_14/cross_table_pretrain/job_status/'
+                                     Prefix='ec2/2022_09_14/cross_table_pretrain/iter_'
                                      )
 for object in response['Contents']:
     print('Deleting', object['Key'])
     s3_client.delete_object(Bucket='automl-benchmark-bingzzhu', Key=object['Key'])
 
-s3 = boto3.client('s3')
-directory_name = 'ec2/2022_09_14/cross_table_pretrain/job_status'
-s3.put_object(Bucket=BUCKET, Key=(directory_name+'/'))
+# s3 = boto3.client('s3')
+# directory_name = 'ec2/2022_09_14/cross_table_pretrain/job_status'
+# s3.put_object(Bucket=BUCKET, Key=(directory_name+'/'))
