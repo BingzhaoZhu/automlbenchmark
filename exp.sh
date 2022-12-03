872a6485fe9611c31a9e2ce209ec2160a3a8e19f
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-03 01:14:15 +0000

    support cross table

--- exp.sh
+++ exp.sh
@@ -3,8 +3,8 @@
 rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-#python runbenchmark.py FTT_AG_hog_ft0 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft0 ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_AG_hog_ft250 ag_finetune mytest1h -m aws -p 520
-#python runbenchmark.py FTT_AG_hog_ft500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft500 ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_AG_hog_ft750 ag_finetune mytest1h -m aws -p 520
-#python runbenchmark.py FTT_AG_hog_ft1000 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft1000 ag_finetune mytest1h -m aws -p 520
--- exp_cross_table.sh
+++ exp_cross_table.sh
@@ -3,8 +3,15 @@
 rm -f ~/.config/automlbenchmark/config.yaml
 cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-#python runbenchmark.py FTT_AG_hog_ft0_lowe ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft0_lowe ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_AG_hog_ft250_lowe ag_finetune mytest1h -m aws -p 520
-#python runbenchmark.py FTT_AG_hog_ft500_lowe ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft500_lowe ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_AG_hog_ft750_lowe ag_finetune mytest1h -m aws -p 520
-#python runbenchmark.py FTT_AG_hog_ft1000_lowe ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_AG_hog_ft1000_lowe ag_finetune mytest1h -m aws -p 520
+
+
+#python runbenchmark.py FTT_AG_hog_ft0_lowi ag_finetune mytest1h -m aws -p 520
+#python runbenchmark.py FTT_AG_hog_ft250_lowi ag_finetune mytest1h -m aws -p 520
+#python runbenchmark.py FTT_AG_hog_ft500_lowi ag_finetune mytest1h -m aws -p 520
+#python runbenchmark.py FTT_AG_hog_ft750_lowi ag_finetune mytest1h -m aws -p 520
+#python runbenchmark.py FTT_AG_hog_ft1000_lowi ag_finetune mytest1h -m aws -p 520
