94adea1e600658b167db0ee67f90cd88bd431a6c
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-16 06:56:47 +0000

    1

--- exp.sh
+++ exp.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
+# rm -f ~/.config/automlbenchmark/config.yaml
+# cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
 python runbenchmark.py FTT_ft0_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft15k_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft30k_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft45k_fewshot ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft250_fewshot ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft500_fewshot ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1000_fewshot ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1500_fewshot ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000_fewshot ag_finetune mytest1h -m aws -p 520
--- exp2.sh
+++ exp2.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-rm -f ~/.config/automlbenchmark/config.yaml
-cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
+# rm -f ~/.config/automlbenchmark/config.yaml
+# cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
 python runbenchmark.py FTT_ft0 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft15k ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft30k ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft45k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft250 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1000 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000 ag_finetune mytest1h -m aws -p 520
