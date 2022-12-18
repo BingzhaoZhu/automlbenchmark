284c4689e69cd37e97d98a45e53caeb39d3fb1c8
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-18 04:33:51 +0000

    1

--- exp3.sh
+++ exp3.sh
@@ -3,9 +3,9 @@
 # rm -f ~/.config/automlbenchmark/config.yaml
 # cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py FTT_ft0_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_light ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft0_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft250_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft500_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1000_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1500_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000_cont ag_finetune mytest1h -m aws -p 520
