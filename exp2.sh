a253515ae51b78ce7f3ba19c8d845864c7603810
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-22 05:36:22 +0000

    1

--- exp2.sh
+++ exp2.sh
@@ -3,9 +3,9 @@
 # rm -f ~/.config/automlbenchmark/config.yaml
 # cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py FTT_ft0 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft0_fewshot_2k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft250_fewshot_2k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft500_fewshot_2k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1000_fewshot_2k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1500_fewshot_2k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000_fewshot_2k ag_finetune mytest1h -m aws -p 520
--- exp3.sh
+++ exp3.sh
@@ -3,9 +3,9 @@
 # rm -f ~/.config/automlbenchmark/config.yaml
 # cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py FTT_ft0_cont ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_cont ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500_cont ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_cont ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_cont ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_cont ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft0_fewshot_1k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft250_fewshot_1k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft500_fewshot_1k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1000_fewshot_1k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft1500_fewshot_1k ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000_fewshot_1k ag_finetune mytest1h -m aws -p 520
