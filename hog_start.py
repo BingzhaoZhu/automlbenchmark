73c0094f739157cdd36e1aed250b3886d8ecd690
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-26 06:42:44 +0000

    1

--- hog_start.py
+++ hog_start.py
@@ -1,17 +1,17 @@
 import boto3
 
 ## delete the pretrained
-# while True:
-#     try:
-#         s3_client = boto3.client('s3')
-#         response = s3_client.list_objects_v2(Bucket='automl-benchmark-bingzzhu',
-#                                              Prefix='ec2/2022_09_14/cross_table_pretrain/pretrain_reconstruction_saint'
-#                                              )
-#         for object in response['Contents']:
-#             print('Deleting', object['Key'])
-#             s3_client.delete_object(Bucket='automl-benchmark-bingzzhu', Key=object['Key'])
-#     except:
-#         break
+while True:
+    try:
+        s3_client = boto3.client('s3')
+        response = s3_client.list_objects_v2(Bucket='automl-benchmark-bingzzhu',
+                                             Prefix='ec2/2022_09_14/cross_table_pretrain/pretrain_reconstruction_25_sum'
+                                             )
+        for object in response['Contents']:
+            print('Deleting', object['Key'])
+            s3_client.delete_object(Bucket='automl-benchmark-bingzzhu', Key=object['Key'])
+    except:
+        break
 
 
 ## delete the init
@@ -33,22 +33,22 @@ import os, torch, json
 
 
 ## save new
-with open("../backbones/FTTx3") as f:
-    kwarg = json.loads(f.read())
-# kwarg["additive_attention"] = True
+# with open("../backbones/FTTx3") as f:
+#     kwarg = json.loads(f.read())
+# # kwarg["additive_attention"] = True
 
-kwarg["row_attention"] = True
-kwarg["row_attention_layer"] = "shared"
+# kwarg["row_attention"] = True
+# kwarg["row_attention_layer"] = "shared"
 
-backbone = FT_Transformer(
-    **kwarg
-)
+# backbone = FT_Transformer(
+#     **kwarg
+# )
 
-checkpoint = {
-    "state_dict": {name: param for name, param in
-                   backbone.state_dict().items()}
-}
-torch.save(checkpoint, os.path.join("./", "pretrained.ckpt"))
-s3 = boto3.resource('s3')
-s3.Bucket('automl-benchmark-bingzzhu').upload_file('./pretrained.ckpt',
-                                                   'ec2/2022_09_14/cross_table_pretrain/pretrained_hogwild_Saint.ckpt')
+# checkpoint = {
+#     "state_dict": {name: param for name, param in
+#                    backbone.state_dict().items()}
+# }
+# torch.save(checkpoint, os.path.join("./", "pretrained.ckpt"))
+# s3 = boto3.resource('s3')
+# s3.Bucket('automl-benchmark-bingzzhu').upload_file('./pretrained.ckpt',
+#                                                    'ec2/2022_09_14/cross_table_pretrain/pretrained_hogwild_Saint.ckpt')
