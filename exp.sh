66596b706dcc37affdb9dd927ddd4c376cd5edbc
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-28 06:28:31 +0000

    1

--- exp.sh
+++ exp.sh
@@ -9,3 +9,6 @@ python runbenchmark.py FTT_ft500_fewshot ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_ft1000_fewshot ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_ft1500_fewshot ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_ft2000_fewshot ag_finetune mytest1h -m aws -p 520
+
+python runbenchmark.py Saint_ft500_light ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft1000_light ag_finetune mytest1h -m aws -p 520
--- exp_hog.sh
+++ exp_hog.sh
@@ -5,9 +5,21 @@
 
 # python runbenchmark.py FTT_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
 # python runbenchmark.py FTT_pretrain_all ag_pretrain mytest8h -m aws -p 180 -f 0
-python runbenchmark.py FTT_pretrain_reconstruction_10 ag_pretrain mytest8h -m aws -p 180 -f 0
-python runbenchmark.py FTT_pretrain_reconstruction_5 ag_pretrain mytest8h -m aws -p 180 -f 0
-python runbenchmark.py FTT_pretrain_reconstruction_1 ag_pretrain mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_10 ag_pretrain mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_5 ag_pretrain mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_1 ag_pretrain mytest8h -m aws -p 180 -f 0
 
-# python runbenchmark.py Saint_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
+python runbenchmark.py Saint_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
 # python runbenchmark.py Fastformer_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
+
+python runbenchmark.py Saint_ft0 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft250 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft1000 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft1500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py Saint_ft2000 ag_finetune mytest1h -m aws -p 520
+
+
+python runbenchmark.py FTT_pretrain_reconstruction_18_task ag_pretrain_18 mytest8h -m aws -p 180 -f 0
+python runbenchmark.py FTT_pretrain_reconstruction_36_task ag_pretrain_36 mytest8h -m aws -p 180 -f 0
+python runbenchmark.py FTT_pretrain_reconstruction_52_task ag_pretrain mytest8h -m aws -p 180 -f 0
