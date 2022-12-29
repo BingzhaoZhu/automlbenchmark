d075592bc60960090ebe84b92de879aad56804d0
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-29 22:33:02 +0000

    1

--- exp_hog.sh
+++ exp_hog.sh
@@ -10,18 +10,12 @@
 # python runbenchmark.py FTT_pretrain_reconstruction_1 ag_pretrain mytest8h -m aws -p 180 -f 0
 
 # python runbenchmark.py Saint_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py Saint_pretrain_supervised ag_pretrain mytest8h -m aws -p 180 -f 0
 # python runbenchmark.py Fastformer_pretrain_reconstruction ag_pretrain mytest8h -m aws -p 180 -f 0
 
-# python runbenchmark.py Saint_ft0 ag_finetune mytest1h -m aws -p 520
-# python runbenchmark.py Saint_ft250 ag_finetune mytest1h -m aws -p 520
-# python runbenchmark.py Saint_ft500 ag_finetune mytest1h -m aws -p 520
-# python runbenchmark.py Saint_ft1000 ag_finetune mytest1h -m aws -p 520
-# python runbenchmark.py Saint_ft1500 ag_finetune mytest1h -m aws -p 520
-# python runbenchmark.py Saint_ft2000 ag_finetune mytest1h -m aws -p 520
-
-python runbenchmark.py FTT_pretrain_reconstruction_18_task ag_pretrain_18 mytest8h -m aws -p 180 -f 0
-python runbenchmark.py FTT_pretrain_reconstruction_36_task ag_pretrain_36 mytest8h -m aws -p 180 -f 0
-python runbenchmark.py FTT_pretrain_reconstruction_52_task ag_pretrain mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_18_task ag_pretrain_18 mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_36_task ag_pretrain_36 mytest8h -m aws -p 180 -f 0
+# python runbenchmark.py FTT_pretrain_reconstruction_52_task ag_pretrain mytest8h -m aws -p 180 -f 0
 
 python runbenchmark.py FTT_pretrain_supervised_blk_1 ag_pretrain mytest8h -m aws -p 180 -f 0
 python runbenchmark.py FTT_pretrain_supervised_blk_2 ag_pretrain mytest8h -m aws -p 180 -f 0
