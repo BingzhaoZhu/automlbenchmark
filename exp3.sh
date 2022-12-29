1946b8889eccfe552acbab4621e2eb8c084e65ff
BingzhaoZhu <bingzhaozhu@gmail.com>
2022-12-29 04:17:58 +0000

    1

--- exp3.sh
+++ exp3.sh
@@ -3,14 +3,12 @@
 # rm -f ~/.config/automlbenchmark/config.yaml
 # cp ./examples/config_gpu.yaml ~/.config/automlbenchmark/config.yaml
 
-python runbenchmark.py FTT_ft0_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft0_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft250_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft500_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1000_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1500_light ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft2000_light ag_finetune mytest1h -m aws -p 520
 
-python runbenchmark.py Saint_ft0_light ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py Saint_ft250_light ag_finetune mytest1h -m aws -p 520
 
 
