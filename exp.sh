ca2fc3501d5a2ab51dce702018dab0263f58e6cd
BingzhaoZhu <bingzhaozhu@gmail.com>
2023-01-02 01:19:30 +0000

    1

--- exp.sh
+++ exp.sh
@@ -10,14 +10,17 @@
 # python runbenchmark.py FTT_ft1500_fewshot ag_finetune mytest1h -m aws -p 520
 # python runbenchmark.py FTT_ft2000_fewshot ag_finetune mytest1h -m aws -p 520
 
-python runbenchmark.py FTT_ft0_with_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_with_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500_with_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_with_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_with_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft0_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft250_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft500_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1000_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1500_with_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft2000_with_cls ag_finetune mytest1h -m aws -p 520
 
-python runbenchmark.py FTT_ft500_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft500_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1000_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1500_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft2000_fewshot ag_finetune mytest1h -m aws -p 520
+
+python runbenchmark.py FTT_ft1500 ag_finetune mytest1h -m aws -p 520
+python runbenchmark.py FTT_ft2000 ag_finetune mytest1h -m aws -p 520
--- exp2.sh
+++ exp2.sh
@@ -10,14 +10,14 @@
 # python runbenchmark.py FTT_ft1500 ag_finetune mytest1h -m aws -p 520
 # python runbenchmark.py FTT_ft2000 ag_finetune mytest1h -m aws -p 520
 
-python runbenchmark.py FTT_ft0_only_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_only_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft500_only_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1000_only_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft1500_only_cls ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft2000_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft0_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft250_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft500_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1000_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft1500_only_cls ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft2000_only_cls ag_finetune mytest1h -m aws -p 520
 
 python runbenchmark.py FTT_ft0 ag_finetune mytest1h -m aws -p 520
 python runbenchmark.py FTT_ft250 ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft0_fewshot ag_finetune mytest1h -m aws -p 520
-python runbenchmark.py FTT_ft250_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft0_fewshot ag_finetune mytest1h -m aws -p 520
+# python runbenchmark.py FTT_ft250_fewshot ag_finetune mytest1h -m aws -p 520
